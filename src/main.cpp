f6fecad022aab19bebeff5e1e8b27030ad6e84bf
dongkc <dongkechang@gmail.com>
2015-07-08 16:44:09 +0800

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,6 @@ int main(int argc, char** argv)
   Session session("MySQL", "host=localhost;port=3306;db=test;user=root;password=dongkai;compress=true;auto-reconnect=true");
 
   LOG(INFO) << "Database connect successfull";
-  //session << "DROP TABLE IF EXISTS gps_data", now;
 
   session << "CREATE TABLE IF NOT EXISTS gps_data(lock_id VARCHAR(12), timestamp TIMESTAMP, message_id VARCHAR(4), frame blob)", now;
   LOG(INFO) << "CREATE TABLE DONE";
@@ -101,53 +100,15 @@ int main(int argc, char** argv)
   File f("raw_data.dat");
   SharedMemory mem(f, SharedMemory::AM_READ);
 
+  LockFrame frame;
+  bool error = false;
   for (char* ptr = mem.begin(); ptr != mem.end(); ++ptr) {
-
+    ptr = parse(ptr, mem.end(), frame, error);
+    if (error) {
+      LOG(ERROR) << "Parse frame error";
+      break;
+    }
   }
 
-#if 0
-  // insert some rows
-  Person person =
-  {
-    "Bart Simpson",
-    "Springfield",
-    12
-  };
-
-  Statement insert(session);
-  insert << "INSERT INTO Person VALUES(?, ?, ?);",
-         use(person.name),
-         use(person.address),
-         use(person.age);
-
-  insert.execute();
-
-  person.name    = "Lisa Simpson";
-  person.address = "Springfield";
-  person.age     = 10;
-
-  insert.execute();
-
-  person.name    = "Lisa Simpson";
-  person.address = "Springfield";
-  person.age     = 14;
-
-  insert.execute();
-
-  // a simple query
-  Statement select(session);
-  select << "SELECT Name, Address, Age FROM Person;",
-         into(person.name),
-         into(person.address),
-         into(person.age),
-         range(0, 1); //  iterate over result set one row at a time
-
-  while (!select.done())
-  {
-    select.execute();
-    std::cout << person.name << " " << person.address << " " << person.age << std::endl;
-  }
-
-#endif
   return 0;
 }
