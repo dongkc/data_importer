f449e8a61d613e6dc2a112a82f4aa0f41eb02a85
dongkc <dongkechang@gmail.com>
2015-07-08 16:38:47 +0800

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include <Poco/File.h>
+#include <Poco/SharedMemory.h>
+#include <Poco/DataTimeParser.h>
+
 #include "Poco/Data/Session.h"
 #include "Poco/Data/MySQL/Connector.h"
 #include <vector>
@@ -9,28 +13,97 @@ using namespace Poco::Data::Keywords;
 using Poco::Data::Session;
 using Poco::Data::Statement;
 
-struct Person
+using namespace Poco;
+
+std::string to_str(const unsigned char byte)
 {
-  std::string name;
-  std::string address;
-  int         age;
+  char buf[2];
+  char tmp  = ((byte & 0xF0) >> 4);
+  if (tmp > 9) {
+    buf[0] = tmp - 10 + 'A';
+  } else {
+    buf[0] = tmp + '0';
+  }
+  tmp = byte & 0x0F;
+  if (tmp > 9) {
+    buf[1] = tmp - 10 + 'A';
+  } else {
+    buf[1] = tmp + '0';
+  }
+  std::string res(buf, 2);
+
+  return res;
+}
+
+struct LockFrame
+{
+  std::string    lock_id;
+  std::string    message_id;
+  Poco::DateTime datetime;
+  std::string    frame;
 };
+
+//解析一个frame，移动指针到下一个frame的开始，如果出错，设置标志位error
+char* parse(char* begin, char* end, LockFrame& lock_frame, bool& error)
+{
+  char* offset = begin;
+
+  if (*offset != 0xFF) {
+    LOG(ERROR) << "Parse error, not begin with magic id 0xFF";
+    error = true;
+    return begin;
+  }
+
+  string timestamp(++offset, 19);
+  DateTime datetime;
+  int timezone;
+  if (!DateTimeParser::tryParse(DateTimeFormat::SORTABLE_FORMAT, timestamp, lock_frame.datetime, timezone)) {
+    LOG(ERROR) << "Parse timestamp error";
+    error = true;
+    return begin;
+  }
+
+  offset += 19;
+  uint16_t len;
+  memcpy(&len, offset, 2);
+
+  if (*offset != 0x7E || *(offset + len) != 0x7E) {
+    LOG(ERROR) << "Invalid frame, not begin or end with 0x7E";
+    error = true;
+    return begin;
+  }
+
+  offset++;
+  lock_frame.message_id_ = to_str(*offset) + to_str(*(offset + 1));
+
+  string phone_num;
+  for (int i = 4; i < 10; ++i) {
+    phone_num += to_str(*(offset + i));
+  }
+
+  lock_frame.lock_id = phone_num;
+  lock_frame.frame = string(--offset, len);
+
+  return offset + len;
+}
 
 int main(int argc, char** argv)
 {
   Poco::Data::MySQL::Connector::registerConnector();
-
-  // create a session
   Session session("MySQL", "host=localhost;port=3306;db=test;user=root;password=dongkai;compress=true;auto-reconnect=true");
 
   LOG(INFO) << "Database connect successfull";
-  // drop sample table, if it exists
-  session << "DROP TABLE IF EXISTS gps_data", now;
+  //session << "DROP TABLE IF EXISTS gps_data", now;
 
-  // (re)create table
-  //session << "CREATE TABLE Person (Name TIMESTAMP, Address VARCHAR(100), Age INTEGER(3))", now;
-  session << "CREATE TABLE gps_data(lock_id VARCHAR(10), timestamp TIMESTAMP, message_id VARCHAR(5), frame blob)", now;
+  session << "CREATE TABLE IF NOT EXISTS gps_data(lock_id VARCHAR(12), timestamp TIMESTAMP, message_id VARCHAR(4), frame blob)", now;
   LOG(INFO) << "CREATE TABLE DONE";
+
+  File f("raw_data.dat");
+  SharedMemory mem(f, SharedMemory::AM_READ);
+
+  for (char* ptr = mem.begin(); ptr != mem.(); ++ptr) {
+
+  }
 
 #if 0
   // insert some rows
